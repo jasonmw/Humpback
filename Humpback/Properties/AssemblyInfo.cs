c163fe7c6486b1d80a8c8fd7297e55a7e5987752
Peter Schmidt <pschmidt@previser.com>
2023-11-20 10:15:46 -0500

    Created shared assembly file.

--- Humpback/Properties/AssemblyInfo.cs
+++ Humpback/Properties/AssemblyInfo.cs
@@ -21,17 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fbe207ef-760f-40d3-9821-604602de11de")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
-[assembly: InternalsVisibleTo("Humpback.Tests")]
