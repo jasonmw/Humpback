912e69fb13269d791a0c4127ec18c40bb6f9441c
Peter Schmidt <pschmidt@previser.com>
2023-12-11 16:21:14 -0500

    Added comments.

--- Humpback.Smo/Executor.cs
+++ Humpback.Smo/Executor.cs
@@ -5,7 +5,12 @@ namespace Humpback.Smo
 {
     public static class Executor
     {
-
+        /// <summary>
+        /// Execute TSQL strings using Microsoft.SqlServer.Management.Smo.
+        /// </summary>
+        /// <param name="connection_string">Config connection string</param>
+        /// <param name="sql">Compiled TSQL script.</param>
+        /// <returns>number of records affected.</returns>
         public static int Execute(string connection_string, string sql)
         {
             var connection = new Microsoft.Data.SqlClient.SqlConnection(connection_string);
